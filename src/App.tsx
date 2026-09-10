7bb72dffbc731de01fb78fc7e55f52b78de417c1
Chief of Staff <chief@local>
2026-09-10 14:18:58 +0000

    Remove accidental ResidencyMap onActivatePath prop
    Keep Interview Reset PATH clear without depending on uncommitted map-query WIP.

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,6 @@ export default function App() {
           <ResidencyMap
             activePath={activePath}
             onClearPath={clearPath}
-            onActivatePath={activatePath}
             interviewedNodes={interviewedNodes}
             interviewedEdges={interviewedEdges}
           />
